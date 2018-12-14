7de93bea7abb06cdd5f538c443391e10225d4961
cofface <cofface@foxmail.com>
2018-12-14 10:46:59 +0800

    修改编译环境

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-set NDK="E:\android_soft\android-ndk-r11c-windows-x86_64\android-ndk-r11c\ndk-build"
+set NDK="D:\android_tools\android-ndk-r11c-windows-x86_64\android-ndk-r11c\ndk-build"
 call %NDK% NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=Android.mk
 copy libs\armeabi\nohup .\nohup
 call %NDK% NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=Android.mk clean
